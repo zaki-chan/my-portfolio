62122eaf9fa0287ab1299c45d58672ebc3054ad4
zaki-chan <naotti0926@gmail.com>
2024-12-30 17:57:46 +0900

    [fix] change-branch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,7 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='header'>
-      <h1>Title of the portfolio</h1>
-      <nav className="menu">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Skill</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
+      
     </div>
   )
 }
