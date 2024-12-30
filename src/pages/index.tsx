fc38a590c6ac156e21300e14c40dd5c548b5ed6f
zaki-chan <naotti0926@gmail.com>
2024-12-30 18:52:06 +0900

    [Implement] Header html
    I added the nav menu and links to pages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from '../components/App';
+import "../styles/index.css";
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
@@ -10,6 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
         <BrowserRouter>
             <App />
         </BrowserRouter>
