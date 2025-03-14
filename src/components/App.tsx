8b43c21252be95fa676ade806c759535c5c02e97
zaki-chan <naotti0926@gmail.com>
2025-03-14 20:21:41 +0900

    [add]add-tailwindcss

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 
+import "../styles/index.css";
+
 const App = () => {
   return (
     <>
       <Header />
-      <div>Hello World!</div>
+      <div className="font-bold underline">Hello World!</div>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from '../components/App';
+import "../styles/index.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
